eaf53c6e05199e5f63215e5a5eb67b3de07489e7
tizgane <kplatkowski@outlook.com>
2019-12-08 20:14:36 +0100

    Add part2 of d6

--- aoc6.py
+++ aoc6.py
@@ -3,7 +3,8 @@ import networkx as nx
 data = [i.split(')') for i in open('input.txt', 'r').read().splitlines()]
 orbits = {planet[1]: [planet[0]] for planet in data}
 
-graph = nx.DiGraph(orbits)
+graph = nx.Graph(orbits)
 
 num = sum(len(nx.shortest_path(graph, planet, "COM")[1:]) for planet in graph.nodes())
 print("1:", num)
+print("2:", len(nx.shortest_path(graph, "YOU", "SAN")[1:-1]) - 1)
