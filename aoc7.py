0946aa1be317d811671d5fff10a200aa67d0c945
tizgane <kplatkowski@outlook.com>
2019-12-09 11:06:37 +0100

    Add solution for Day7.2

--- aoc7.py
+++ aoc7.py
@@ -21,18 +21,21 @@ class DataPointer:
         finally:
             self.__idx += 1
 
+
 class Computer:
 
-    def __init__(self, data, i1, i2):
+    def __init__(self, data, phase):
         self.__config(data)
-        self.__input = (i for i in [i1, i2])
-        self.__run()
+        self.__halted = False
+        self.__input = [phase]
 
     def __config(self, data):
         self.__data = data
         self.__data_ptr = DataPointer(self.__data)
 
-    def __run(self):
+    def run(self, inp):
+        self.__input.append(inp)
+
         while((i := self.__shift()) != 99):
             opcode, mode = self.__parseInstruction(str(i))
             if (opcode == 1):
@@ -42,7 +45,8 @@ class Computer:
             elif (opcode == 3):
                 self.__write()
             elif (opcode == 4):
-                self.__read(mode)
+                self.__lastRead = self.__read(mode)
+                return self.__lastRead
             elif (opcode == 5):
                 self.__jumpIf(op.ne, mode)
             elif (opcode == 6):
@@ -51,6 +55,10 @@ class Computer:
                 self.__storeOneIf(op.lt, mode)
             elif (opcode == 8):
                 self.__storeOneIf(op.eq, mode)
+
+        self.__halted = True
+        return self.__lastRead
+
 
     def __shift(self):
         return next(self.__data_ptr)
@@ -73,10 +81,10 @@ class Computer:
         self.__data[self.__shift()] = operation(pos1, pos2)
 
     def __write(self):
-        self.__data[self.__shift()] = next(self.__input)
+        self.__data[self.__shift()] = self.__input.pop(0)
 
     def __read(self, mode):
-        self.__output = self.__getData(mode)
+        return self.__getData(mode)
 
     def __jumpIf(self, operation, mode):
         if (operation(self.__getData(mode[0]), 0)):
@@ -99,20 +107,31 @@ class Computer:
     def getResult(self):
         return self.__data[0]
 
-    def getOutput(self):
-        return self.__output
-
+    def wasHalted(self):
+        return self.__halted
 
 data = [int(i) for i in open('input.txt', 'r').readline().split(',')]
 
-
-def getAmplifierOutput(sequence, inp):
-    if not sequence:
+def getAmplifierOutput(sequence_iterator, inp):
+    try:
+        amp = next(sequence_iterator)
+    except StopIteration:
         return inp
 
-    output = Computer(data.copy(), sequence.pop(0), inp).getOutput()
-    return getAmplifierOutput(sequence, output)
+    return getAmplifierOutput(sequence_iterator, Computer(data.copy(), amp).run(inp))
 
-
-num = max(getAmplifierOutput(list(amp), 0) for amp in it.permutations(range(5)))
+num = max(getAmplifierOutput(iter(sequence), 0) for sequence in it.permutations(range(5)))
 print("1:", num)
+
+def getLoopedAmplifierOutput(sequence):
+    amplifiers = [Computer(data.copy(), sequence[i]) for i in range(5)]
+
+    out = 0
+    for i in it.cycle(range(5)):
+        out = amplifiers[i].run(out)
+        if (all(amplifier.wasHalted() for amplifier in amplifiers)):
+            break
+    return out
+
+num = max(getLoopedAmplifierOutput(sequence) for sequence in it.permutations(range(5, 10)))
+print("2:", num)
